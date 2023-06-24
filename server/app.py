764ccf53a6e4020b19e487f9260b3ce089cbfa74
Nyaromollo <omolloenter@gmail.com>
2023-06-24 19:56:02 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -20,19 +20,67 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
 
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
 
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+    response = make_response(
+        bakeries_serialized,
+        200
+    )
+    return response
 
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+
+
+@app.route('/bakeries/<int:id>', methods=['GET'])
+def get_bakery(id):
+    bakery = Bakery.query.get_or_404(id)
+    bakery_data = {
+        'id': bakery.id,
+        'name': bakery.name,
+        'location': bakery.location,
+        'baked_goods': []
+    }
+    for baked_good in bakery.baked_goods:
+        baked_good_data = {
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price
+            # Additional baked good properties...
+        }
+        
+        bakery_data['baked_goods'].append(baked_good_data)
+    return jsonify(bakery_data)
+
+@app.route('/baked_goods/by_price', methods=['GET'])
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = []
+    for baked_good in baked_goods:
+        baked_good_data = {
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price
+            # Additional baked good properties...
+        }
+        baked_goods_list.append(baked_good_data)
+    return jsonify(baked_goods_list)
+
+@app.route('/baked_goods/most_expensive', methods=['GET'])
+def get_most_expensive_baked_good():
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        baked_good_data = {
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price
+            # Additional baked good properties...
+        }
+        return jsonify(baked_good_data)
+    else:
+        return jsonify({'message': 'No baked goods found.'}), 404
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,20 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
-
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
 db = SQLAlchemy(metadata=metadata)
-
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
-
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    def __repr__ (self):
+        return f'<Bakery {self.name}>'
+    
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
-
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__ (self):
+        return f'<BakedGood {self.name} for {self.price}>'
